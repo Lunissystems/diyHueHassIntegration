0264babbfaf83611970bb4322fe3e6ece9461cef
Lunissystems <33608753+Lunissystems@users.noreply.github.com>
2021-06-20 18:17:00 +0200

    Add files via upload

--- custom_components/diyHueHassIntegration/__init__.py
+++ custom_components/diyHueHassIntegration/__init__.py
@@ -1,5 +1,5 @@
+import requests
 from homeassistant.components import light
-from . import diyHueLight
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_LIGHTS, CONF_NAME
@@ -19,6 +19,26 @@ async def async_setup(hass: HomeAssistant, config):
 async def async_setup_entry(hass, config_entry, asnyc_add_devices):
 
     device = hass.data[DOMAIN][config_entry.entry_id]
-    asnyc_add_devices(diyHueLight(device, config_entry, light))
+    asnyc_add_devices(diyHueEntity(device, config_entry))
 
     return True
+
+
+class diyHueEntity:
+    def __init__(self, device, config):
+
+        self._config = config
+        self._state = None
+        self.lights = config.number_of_lights
+        self.IP = config.ipaddress
+        self.addr = "http://" + self.IP + "/state"
+
+    def turn_on(self):
+        requests.post(
+            url=self.addr, params={"on": True, "bri": 255, "xy": [0.53, 0, 21]}
+        )
+
+    def turn_off(self):
+        requests.post(
+            url=self.addr, params={"on": False, "bri": 0, "xy": [0.53, 0, 21]}
+        )
