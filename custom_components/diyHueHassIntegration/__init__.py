1c58aef0cc00ec196cfcd5702cce054995db9e80
Lunissystems <33608753+Lunissystems@users.noreply.github.com>
2021-06-20 18:54:07 +0200

    Add files via upload

--- custom_components/diyHueHassIntegration/__init__.py
+++ custom_components/diyHueHassIntegration/__init__.py
@@ -19,7 +19,6 @@ async def async_setup(hass: HomeAssistant, config):
 async def async_setup_entry(hass, config_entry, async_add_devices):
 
     device = hass.data[DOMAIN][config_entry.entry_id]
-    async_add_devices(diyHueEntity(device, config_entry))
 
     return True
 
